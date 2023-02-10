81007a66a8e3702d727f0b0fd1ab7345f7227ca0
Shin'ichi Ohno <ohno@cookpad.com>
2023-02-10 14:12:38 +0000

    Refactor: RoonNuimoIntegration

--- src/app/integrations/roonNuimoIntegration.ts
+++ src/app/integrations/roonNuimoIntegration.ts
@@ -1,80 +1,91 @@
-import {IPublishPacket} from "async-mqtt";
-import {Broker} from "../broker.js";
-import {IntegrationInterface} from "./interface.js";
+import { IPublishPacket } from "async-mqtt";
+import { Broker } from "../broker.js";
+import { IntegrationInterface } from "./interface.js";
 
 export class RoonNuimoIntegration implements IntegrationInterface {
-    private readonly commandTopic: string;
-    private readonly operationTopic: string;
-    private readonly broker: Broker;
-    private readonly volumeSetTopic: string;
-    private readonly roonStateTopic: string;
-    private readonly roonVolumeTopic: string;
-    private readonly topicsToSubscribe: string[];
-    private readonly nuimoReactionTopic: string;
+  private readonly commandTopic: string;
+  private readonly operationTopic: string;
+  private readonly broker: Broker;
+  private readonly volumeSetTopic: string;
+  private readonly roonStateTopic: string;
+  private readonly roonVolumeTopic: string;
+  private readonly topicsToSubscribe: string[];
+  private readonly nuimoReactionTopic: string;
 
-    constructor(options: { nuimo: string; zone: string; output: string; broker: Broker }) {
-        this.operationTopic = `nuimo/${options.nuimo}/operation`;
-        this.commandTopic = `roon/${options.zone}/command`;
-        this.volumeSetTopic = `roon/${options.zone}/outputs/${options.output}/volume/set/relative`
-        this.roonStateTopic = `roon/${options.zone}/state`
-        this.roonVolumeTopic = `roon/${options.zone}/outputs/${options.output}/volume/percent`
-        this.topicsToSubscribe = [this.operationTopic, this.roonStateTopic, this.roonVolumeTopic];
-        this.nuimoReactionTopic = `nuimo/${options.nuimo}/reaction`;
-        this.broker = options.broker;
-    }
+  constructor(options: {
+    nuimo: string;
+    zone: string;
+    output: string;
+    broker: Broker;
+  }) {
+    this.operationTopic = `nuimo/${options.nuimo}/operation`;
+    this.commandTopic = `roon/${options.zone}/command`;
+    this.volumeSetTopic = `roon/${options.zone}/outputs/${options.output}/volume/set/relative`;
+    this.roonStateTopic = `roon/${options.zone}/state`;
+    this.roonVolumeTopic = `roon/${options.zone}/outputs/${options.output}/volume/percent`;
+    this.topicsToSubscribe = [
+      this.operationTopic,
+      this.roonStateTopic,
+      this.roonVolumeTopic,
+    ];
+    this.nuimoReactionTopic = `nuimo/${options.nuimo}/reaction`;
+    this.broker = options.broker;
+  }
 
-    up(): Promise<any> {
-        this.resolveRoute();
-        return this.broker.subscribe(this.topicsToSubscribe);
-    }
+  up(): Promise<any> {
+    return this.broker.subscribe(this.topicsToSubscribe, this.messageCB);
+  }
 
-    down() {
-        return this.broker.unsubscribe(this.topicsToSubscribe);
-    }
+  down() {
+    return this.broker.unsubscribe(this.topicsToSubscribe);
+  }
 
-    private command(payload: string) {
-        this.broker.publish(this.commandTopic, payload)
-    }
+  private command(payload: string) {
+    this.broker.publish(this.commandTopic, payload);
+  }
 
-    private nuimoReaction(payload: string) {
-        this.broker.publish(this.nuimoReactionTopic, payload);
-    }
+  private nuimoReaction(payload: string) {
+    this.broker.publish(this.nuimoReactionTopic, payload);
+  }
 
-    private setVolume(volume: number) {
-        const relativeVolume = volume * 80;
-        this.broker.publish(this.volumeSetTopic, relativeVolume.toString());
-    }
+  private setVolume(volume: number) {
+    const relativeVolume = volume * 80;
+    this.broker.publish(this.volumeSetTopic, relativeVolume.toString());
+  }
 
-    private resolveRoute() {
-        this.broker.on("message", (topic: string, payload: Buffer, _: IPublishPacket) => {
-            switch (topic) {
-                case this.operationTopic:
-                    switch (JSON.parse(payload.toString()).subject) {
-                        case "select":
-                            this.command("playpause")
-                            break;
-                        case "rotate":
-                            this.setVolume(JSON.parse(payload.toString()).parameter[0])
-                            break;
-                        case "swipeRight":
-                            this.command("next")
-                            break;
-                        case "swipeLeft":
-                            this.command("previous")
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case this.roonStateTopic:
-                    this.nuimoReaction(JSON.stringify({status: payload.toString()}));
-                    break;
-                case this.roonVolumeTopic:
-                    this.nuimoReaction(JSON.stringify({status: "volumeChange", percentage: payload.toString()}));
-                    break;
-                default:
-                    break;
-            }
-        })
+  messageCB = (topic: string, payload: Buffer, _: IPublishPacket) => {
+    switch (topic) {
+      case this.operationTopic:
+        switch (JSON.parse(payload.toString()).subject) {
+          case "select":
+            this.command("playpause");
+            break;
+          case "rotate":
+            this.setVolume(JSON.parse(payload.toString()).parameter[0]);
+            break;
+          case "swipeRight":
+            this.command("next");
+            break;
+          case "swipeLeft":
+            this.command("previous");
+            break;
+          default:
+            break;
+        }
+        break;
+      case this.roonStateTopic:
+        this.nuimoReaction(JSON.stringify({ status: payload.toString() }));
+        break;
+      case this.roonVolumeTopic:
+        this.nuimoReaction(
+          JSON.stringify({
+            status: "volumeChange",
+            percentage: payload.toString(),
+          }),
+        );
+        break;
+      default:
+        break;
     }
+  };
 }
--- tests/app/integrations/roonNuimoIntegration.test.ts
+++ tests/app/integrations/roonNuimoIntegration.test.ts
@@ -1,54 +1,68 @@
-import {RoonNuimoIntegration} from "app/integrations/roonNuimoIntegration";
-import {Broker} from "app/broker";
-import {BrokerConfig} from "app/brokerConfig";
+import { RoonNuimoIntegration } from "app/integrations/roonNuimoIntegration";
+import { Broker } from "app/broker";
+import { BrokerConfig } from "app/brokerConfig";
 import spyOn = jest.spyOn;
 
-jest.mock("app/broker")
-
 describe("RoonNuimoIntegration", () => {
-    it("", async () => {
-        const b = new Broker(new BrokerConfig())
-        const i = new RoonNuimoIntegration({
-            nuimo: "xxx",
-            zone: "yyy",
-            output: "zzz",
-            broker: b
-        })
+  it("", async () => {
+    const b = new Broker(new BrokerConfig());
+    const i = new RoonNuimoIntegration({
+      nuimo: "xxx",
+      zone: "yyy",
+      output: "zzz",
+      broker: b,
+    });
 
-        spyOn(b, "subscribe").mockImplementation(() => new Promise((x, _y) => x(undefined)))
-        const spy = spyOn(b, "on")
+    b.connect = () => new Promise((x, _y) => x(undefined));
+    b.publish = jest.fn();
+    b["on"] = jest.fn();
+    const spySubscription = spyOn(b, "subscribe");
 
-        await i.up().then(() => {
-            expect(b.subscribe).toHaveBeenCalledWith([
-                "nuimo/xxx/operation",
-                "roon/yyy/state",
-                "roon/yyy/outputs/zzz/volume/percent",
-            ]);
+    await i.up().catch((reason) => {
+      //Client is disconnected so always rejected
+      expect(reason).toBe(
+        "Client is not initiated for this broker. Call connect() before subscribe.",
+      );
+      expect(b.subscribe).toHaveBeenCalled();
+      expect(spySubscription.mock.calls[0][0]).toEqual([
+        "nuimo/xxx/operation",
+        "roon/yyy/state",
+        "roon/yyy/outputs/zzz/volume/percent",
+      ]);
 
-            expect(spy.mock.calls[0][0]).toBe("message");
+      const cb = spySubscription.mock.calls[0][1];
+      [
+        ["select", "playpause"],
+        ["swipeRight", "next"],
+        ["swipeLeft", "previous"],
+      ].forEach((i) => {
+        cb("nuimo/xxx/operation", JSON.stringify({ subject: i[0] }));
+        expect(b.publish).toHaveBeenLastCalledWith("roon/yyy/command", i[1]);
+      });
 
-            const cb = spy.mock.calls[0][1];
+      cb(
+        "nuimo/xxx/operation",
+        JSON.stringify({ subject: "rotate", parameter: [10, 0] }),
+      );
+      expect(b.publish).toHaveBeenLastCalledWith(
+        "roon/yyy/outputs/zzz/volume/set/relative",
+        "800",
+      );
 
-            [
-                ["select", "playpause"],
-                ["swipeRight", "next"],
-                ["swipeLeft", "previous"],
-            ].forEach(i => {
-                cb("nuimo/xxx/operation", JSON.stringify({subject: i[0]}));
-                expect(b.publish).toHaveBeenLastCalledWith("roon/yyy/command", i[1]);
-            })
+      cb("roon/yyy/state", "stopped");
+      expect(b.publish).toHaveBeenLastCalledWith(
+        "nuimo/xxx/reaction",
+        JSON.stringify({ status: "stopped" }),
+      );
 
-            cb("nuimo/xxx/operation", JSON.stringify({subject: "rotate", parameter: [10, 0]}));
-            expect(b.publish).toHaveBeenLastCalledWith("roon/yyy/outputs/zzz/volume/set/relative", "800");
-
-            cb("roon/yyy/state", "stopped")
-            expect(b.publish).toHaveBeenLastCalledWith("nuimo/xxx/reaction", JSON.stringify({status: "stopped"}));
-
-            cb("roon/yyy/outputs/zzz/volume/percent", 10)
-            expect(b.publish).toHaveBeenLastCalledWith("nuimo/xxx/reaction", JSON.stringify({
-                status: "volumeChange",
-                percentage: "10"
-            }));
-        })
-    })
-})
+      cb("roon/yyy/outputs/zzz/volume/percent", 10);
+      expect(b.publish).toHaveBeenLastCalledWith(
+        "nuimo/xxx/reaction",
+        JSON.stringify({
+          status: "volumeChange",
+          percentage: "10",
+        }),
+      );
+    });
+  });
+});
